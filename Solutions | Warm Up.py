74ac91f50561bb765871f693a498ecfff3456372
Anahad <86925594+Anahad06@users.noreply.github.com>
2022-07-07 14:01:36 -0400

    Update Solutions | Warm Up.py
    solutions for all

--- Solutions | Warm Up.py
+++ Solutions | Warm Up.py
@@ -1,6 +1,4 @@
-#1
-
-# Factorial 
+#1: Factorial Solution 
 x = int(input("Factorial of: "))
 val=1
 
@@ -8,3 +6,22 @@ for i in range(2, x + 1):
     val *= i
     
 print("Solution:",val)
+
+
+#2: Multiplication Table 
+
+x = int(input("Multiplication Table Of: "))
+
+for i in range(1, 11):
+    print(i, "x", x, "=", x * i)
+
+ 
+#3: Cube 
+
+x = int(input("Find The Cubes Until: "))
+
+for i in range(1, x + 1):
+    print("Cube of", i, "=", i ** 3)
+
+# MOVE ON TO NEXT WORKSHEET ONCE FINISHED. BE SURE TO REDO PROBLEMS THAT YOU HAVE DOUBT WITH!
+
